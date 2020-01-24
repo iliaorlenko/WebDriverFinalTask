6fdd26232d21d5ff39678faa90fecfc0d822280b
Ilya Orlenko <ilia.orlenko@gmail.com>
2020-01-24 17:04:44 +0300

    Added /bin and /obj directories to .gitignore

---
+++ WebDriverFinalTask/TestData/CurrentValues.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebDriverFinalTask.TestData
+{
+    public static class CurrentValues
+    {
+        public static string EmailSubject { get; set; }
+        public static string EmailBody { get; set; }
+    }
+}
